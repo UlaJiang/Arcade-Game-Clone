d85f6e6f093885d2f3df12d1e438b4fef1f2066c
UlaJiang <ulajiang0419@gmail.com>
2018-02-28 21:29:48 +1100

    first commit

---
+++ js/app.js
@@ -0,0 +1,110 @@
+// 这是我们的玩家要躲避的敌人 
+var Enemy = function(x,y) {
+    // 要应用到每个敌人的实例的变量写在这里
+    this.x = -10;
+    this.y = 60 + 85 * Math.floor(Math.random() * 3);
+    this.speed = (Math.random()+1) * 150;
+    // 敌人的图片，用一个我们提供的工具函数来轻松的加载文件
+    this.sprite = 'images/enemy-bug.png';
+};
+
+
+// 此为游戏必须的函数，用来更新敌人的位置
+// 参数: dt ，表示时间间隙
+Enemy.prototype.update = function(dt) {
+    // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
+    // 都是以同样的速度运行的
+    this.x += this.speed * dt;
+    //判断allEnemies数组里面是否有enemy已经爬到最右，如果是的话就从数组中删除
+    for(var i = 0; i < allEnemies.length; i++){
+        if(allEnemies[i].x >= 502){
+            allEnemies.splice(i, 1);
+            break;
+        }
+    }
+    //处理碰撞 
+    for(var i = 0; i < allEnemies.length; i++){
+        if ((Math.abs(allEnemies[i].y - player.y) < 40) && (Math.abs(allEnemies[i].x - player.x) < 60)){
+            player.reset();
+        }
+    }
+
+};
+
+// 此为游戏必须的函数，用来在屏幕上画出敌人，
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// 现在实现你自己的玩家类
+var Player = function(){
+    this.x = 200;
+    this.y = 410;
+    this.sprite = 'images/char-boy.png';
+}
+// 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
+Player.prototype.update = function() {
+        //判断游戏什么时候成功
+        if (this.y < 70) {
+            alert('Congratulations, you win the game!');
+            this.reset();
+        }
+        //控制玩家在指定的范围内移动，超出范围都会重置游戏
+        if( this.x > 400 || this.x < 0 || this.y > 410){
+            this.reset();
+        }
+
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+}
+//处理玩家按上下左右键的时候位置移动
+Player.prototype.handleInput = function(num) {
+        if (num === 'up'){
+            this.update();
+            this.y = this.y - 83; 
+        }
+        else if (num === 'down') {
+            this.update();
+            this.y = this.y + 83;
+        }
+        else if (num === 'left') {
+            this.update();
+            this.x = this.x - 100;
+        }
+        else if (num === 'right') {
+            this.update();
+            this.x = this.x + 100;
+        }
+}
+//重置游戏，及重置玩家位置至初始状态
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 410;
+}
+
+// 实例化所有Enemy对象
+var allEnemies = new Array();
+
+//每隔一秒生成一个新的enemy对象，把该对象都放进一个叫 allEnemies 的数组里面
+setInterval(function(){
+        var enemy = new Enemy();
+        allEnemies.push(enemy);
+}, 1000);
+
+var player = new Player();
+
+// 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Player.handleInput()
+// 方法里面。你不需要再更改这段代码了。
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
